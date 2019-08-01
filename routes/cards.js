c237766fe7ae99dd300a759ead430d2846dd535f
Balazs Holpar <Balazs.Holpar@continental-corporation.com>
2019-08-01 11:28:46 +0200

    Added answer validation to router.patch

--- routes/cards.js
+++ routes/cards.js
@@ -55,11 +55,18 @@ router.patch("/update/:id", (req, res) => {
     var db = req.db;
     var collection = db.get("cards");
     console.log("Original body", req.body)
-    // update the data specified by the id url parameter in the collection with new data from the request body
-    collection.update({"_id" : req.params.id}, {$set: req.body}, (error) => {
-        // send OK response if data removal was successfull, otherwise send the error message
-        res.json((error === null) ? {isSuccessful: true} : {isSuccessful: false, error: error});
-    });
+    var data = req.body;
+    if(data.hasOwnProperty("answer") && data.answer.length > 0) {
+        // update the data specified by the id url parameter in the collection with new data from the request body
+        collection.update({"_id" : req.params.id}, {$set: req.body}, (error) => {
+            // send OK response if data removal was successfull, otherwise send the error message
+            res.json((error === null) ? {isSuccessful: true} : {isSuccessful: false, error: error});
+        });
+    }
+    else {
+        res.json({isSuccessful: false, error: "INVALID_ANSWER_LENGTH"});
+        console.log("Invalid answer");
+    }
 });
 
 
